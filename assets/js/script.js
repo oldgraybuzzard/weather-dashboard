e58a30f76aafcdaa0f11fc8c332bcb7b7eefc81e
oldgraybuzzard <k_felder@me.com>
2021-09-27 16:27:21 -0400

    secret hidden

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 var token = config.apiToken;
+var cityName = "orlando";
+var stateCode = "fl";
+var country = "usa"
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + stateCode + "," + country + "&appid=" + token;
 
+
+//Fetch to OpenWeather
+// fetch ('https://api.openweathermap.org/data/2.5/weather?q=orlando,fl,usa&appid=02890bdb99719fad40851d0a9b902f85').then(response.json()).then(data => console.log(data));
+fetch (apiUrl).then(response.json()).then(data => console.log(data));
