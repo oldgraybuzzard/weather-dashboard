b51403c96d61942d1abac7bdac14084dc1877251
oldgraybuzzard <k_felder@me.com>
2021-10-03 06:36:29 -0400

    last stand

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // current day and time
 $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY"));
-$("#currentTime").text(dayjs().format('HH : mm A'));
+$("#currentTime").text(dayjs().format('HH:mm'));
 
 // Variables Section
 var token = config.apiToken;
@@ -9,56 +9,60 @@ const apiCityURL = "&appid=" + token
 var cityNameEl = document.getElementById("enter-city");
 var searchCityEl = document.querySelector("#search-button");
 const searchEl = document.getElementById("search-button");
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
 var historyEl = document.getElementById("history");
 
-// search button function
-searchEl.addEventListener("click", function () {
-    const searchTerm = cityNameEl.value.trim();
-    // getWeather(searchTerm);
-    searchHistory.push(searchTerm);
-    localStorage.setItem("city searches", JSON.stringify(searchHistory));
-    // renderSearchHistory();
-})
+// city search
+$("#enter-city").keypress(function(event){
+    // press enter key
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      $("#search-button").click();
+    }
+  });
 
-//display search history
-displaySearchHistory();
-    function displaySearchHistory(){
-        historyEl.innerHTML = "";
-        for (let i = 0; i < searchHistory.length; i++) {
-            const historyList = document.createElement("history");
-                historyList.setAttribute("type", "text");
-                historyList.setAttribute("value", searchHistory[i]);
-                khistoryList.setAttribute("class", "form-control d-block bg-white");
-            }
-            historyEl.append(history);
-        }
-
-
+  // city search history
+cityList();
+function cityList() {
+  $("#history").html("");
+  var cities = JSON.parse(localStorage.getItem("cities")) || [];
+  console.log(cities);
+  for (var i = 0; i < cities.length; i++)
+    $(".list").append('<li class="list-item">' + cities[i] + "</li>");
+  // sets on click for the list of cities in the history list
+  $(".list-item").on("click", function () {
+    city = $(this).text();
+    $("#enter-city").val(city);
+    $("#search-button").click();
+  });
+}
+// localStorage
+function makeList() {
+    var cities = JSON.parse(localStorage.getItem("cities")) || [];
+    if (cities.indexOf(city) === -1) {
+      cities.push(city);
+      console.log(cities);
+    }
+    localStorage.setItem("cities", JSON.stringify(cities));
+    cityList();
+  }
+  // searching a city
+$("#searchBtn").on("click", function () {
+    // get the value of the input from user
+    city = $("#enter-city").val().trim();
+  
+    // clear input box
+    $("#enter-city").val("");
 
 
 //fetch city weather
 var getCityWeather = function(){
-    let cityNameUrl = cityNameEl;
+    var cityNameUrl = cityNameEl;
         fetch(apiUrl + cityNameUrl + apiCityURL).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
+        getCityWeather(response);
+        getCityForecast(response);
+        makeList(response);
         })
-    })
-}
-
-
-// local storage
-function citySearchList(){
-    if (searchHistory === -1) {
-        console.log("need more");
     }
-  localStorage.setItem("city", JSON.stringify(cityNameEl));  
-}
-
-
-//searchhistory
-searchHistory =[];
-
 
 getCityWeather();
